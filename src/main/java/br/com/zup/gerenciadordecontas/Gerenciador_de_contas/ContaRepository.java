ed982755be8b98bb970f915ca4fed7244957efe8
Anna Karolyne <dev.akvc@gmail.com>
2021-11-10 15:21:29 -0300

    Criar classe Repository

---
+++ src/main/java/br/com/zup/gerenciadordecontas/Gerenciador_de_contas/ContaRepository.java
@@ -0,0 +1,6 @@
+package br.com.zup.gerenciadordecontas.Gerenciador_de_contas;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ContaRepository extends CrudRepository<Conta, Integer> {
+}
