222ea50f5ac38fb860c08421b5e9dd4988b6500e
Anna Karolyne <dev.akvc@gmail.com>
2021-11-10 17:10:58 -0300

    Atualizar status ao cadastrar conta

--- src/main/java/br/com/zup/gerenciadordecontas/Gerenciador_de_contas/ContaService.java
+++ src/main/java/br/com/zup/gerenciadordecontas/Gerenciador_de_contas/ContaService.java
@@ -1,9 +1,11 @@
 package br.com.zup.gerenciadordecontas.Gerenciador_de_contas;
 
+import br.com.zup.gerenciadordecontas.Gerenciador_de_contas.enuns.StatusConta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Service
 public class ContaService {
@@ -14,7 +16,14 @@ public class ContaService {
     LocalDate dataDeVencimento;
 
     public void cadastrarConta(Conta conta) {
+        conta.setDataDePagamento(LocalDateTime.now());
         conta.setDataDeVencimento(LocalDate.of(dataDeVencimento.getDayOfMonth(), dataDeVencimento.getMonth(), dataDeVencimento.getYear()));
+        if(dataDeVencimento.isBefore(LocalDate.now())){
+            conta.setStatus(StatusConta.VENCIDA);
+        } else{
+            conta.setStatus(StatusConta.AGUARDANDO);
+        }
+
         contaRepository.save(conta);
     }
 
