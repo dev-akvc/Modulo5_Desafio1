741e08d5ff27b5732f6c57ef709e0b3c4bb25a03
Anna Karolyne <dev.akvc@gmail.com>
2021-11-10 15:53:34 -0300

    Criar metodo para cadastrar conta

--- src/main/java/br/com/zup/gerenciadordecontas/Gerenciador_de_contas/ContaService.java
+++ src/main/java/br/com/zup/gerenciadordecontas/Gerenciador_de_contas/ContaService.java
@@ -3,11 +3,20 @@ package br.com.zup.gerenciadordecontas.Gerenciador_de_contas;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+
 @Service
 public class ContaService {
 
     @Autowired
     private ContaRepository contaRepository;
+
+    LocalDate dataDeVencimento;
+
+    public void cadastrarConta(Conta conta) {
+        conta.setDataDeVencimento(LocalDate.of(dataDeVencimento.getDayOfMonth(), dataDeVencimento.getMonth(), dataDeVencimento.getYear()));
+        contaRepository.save(conta);
+    }
 
 
 }
