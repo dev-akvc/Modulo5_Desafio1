25f29c8ee074341365a508329db7a941e7073813
Anna Karolyne <dev.akvc@gmail.com>
2021-11-10 17:09:40 -0300

    Adicionar anotacoes lombok na classe model

--- src/main/java/br/com/zup/gerenciadordecontas/Gerenciador_de_contas/Conta.java
+++ src/main/java/br/com/zup/gerenciadordecontas/Gerenciador_de_contas/Conta.java
@@ -2,10 +2,17 @@ package br.com.zup.gerenciadordecontas.Gerenciador_de_contas;
 
 import br.com.zup.gerenciadordecontas.Gerenciador_de_contas.enuns.StatusConta;
 import br.com.zup.gerenciadordecontas.Gerenciador_de_contas.enuns.TipoConta;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+@Setter
 
 @Entity
 @Table(name = "contas")
@@ -23,64 +30,5 @@ public class Conta {
     private LocalDate dataDeVencimento;
     @Column(name = "data_de_pagamento")
     private LocalDateTime dataDePagamento;
-
-    public Conta() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public double getValor() {
-        return valor;
-    }
-
-    public void setValor(double valor) {
-        this.valor = valor;
-    }
-
-    public TipoConta getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(TipoConta tipo) {
-        this.tipo = tipo;
-    }
-
-    public StatusConta getStatus() {
-        return status;
-    }
-
-    public void setStatus(StatusConta status) {
-        this.status = status;
-    }
-
-    public LocalDate getDataDeVencimento() {
-        return dataDeVencimento;
-    }
-
-    public void setDataDeVencimento(LocalDate dataDeVencimento) {
-        this.dataDeVencimento = dataDeVencimento;
-    }
-
-    public LocalDateTime getDataDePagamento() {
-        return dataDePagamento;
-    }
-
-    public void setDataDePagamento(LocalDateTime dataDePagamento) {
-        this.dataDePagamento = dataDePagamento;
-    }
 
 }
