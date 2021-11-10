d780388147abae099ccc9639defa69efa92d6209
Anna Karolyne <dev.akvc@gmail.com>
2021-11-10 17:06:50 -0300

    Adicionar anotacoes lombok

--- src/main/java/br/com/zup/gerenciadordecontas/Gerenciador_de_contas/dtos/CadastrarContaDTO.java
+++ src/main/java/br/com/zup/gerenciadordecontas/Gerenciador_de_contas/dtos/CadastrarContaDTO.java
@@ -1,48 +1,19 @@
 package br.com.zup.gerenciadordecontas.Gerenciador_de_contas.dtos;
 
 import br.com.zup.gerenciadordecontas.Gerenciador_de_contas.enuns.TipoConta;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
+@NoArgsConstructor
+@Getter
+@Setter
 public class CadastrarContaDTO {
     private String nome;
     private double valor;
     private TipoConta tipo;
     private LocalDate dataDeVencimento;
-
-    public CadastrarContaDTO() {
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public double getValor() {
-        return valor;
-    }
-
-    public void setValor(double valor) {
-        this.valor = valor;
-    }
-
-    public TipoConta getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(TipoConta tipo) {
-        this.tipo = tipo;
-    }
-
-    public LocalDate getDataDeVencimento() {
-        return dataDeVencimento;
-    }
-
-    public void setDataDeVencimento(LocalDate dataDeVencimento) {
-        this.dataDeVencimento = dataDeVencimento;
-    }
 
 }
