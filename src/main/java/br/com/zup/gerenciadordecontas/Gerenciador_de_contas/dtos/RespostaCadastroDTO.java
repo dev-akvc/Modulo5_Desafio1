725378d25c0468c1ab0e0d34b5668ec1f07a14ec
Anna Karolyne <dev.akvc@gmail.com>
2021-11-10 17:07:36 -0300

    Adicionar anotacoes lombok na classe de resposta

--- src/main/java/br/com/zup/gerenciadordecontas/Gerenciador_de_contas/dtos/RespostaCadastroDTO.java
+++ src/main/java/br/com/zup/gerenciadordecontas/Gerenciador_de_contas/dtos/RespostaCadastroDTO.java
@@ -1,10 +1,16 @@
 package br.com.zup.gerenciadordecontas.Gerenciador_de_contas.dtos;
 
 import br.com.zup.gerenciadordecontas.Gerenciador_de_contas.enuns.TipoConta;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@NoArgsConstructor
+@Getter
+@Setter
 public class RespostaCadastroDTO {
     private int id;
     private String nome;
@@ -12,56 +18,5 @@ public class RespostaCadastroDTO {
     private TipoConta tipo;
     private LocalDate dataDeVencimento;
     private LocalDateTime dataDePagamento;
-
-    public RespostaCadastroDTO() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public double getValor() {
-        return valor;
-    }
-
-    public void setValor(double valor) {
-        this.valor = valor;
-    }
-
-    public TipoConta getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(TipoConta tipo) {
-        this.tipo = tipo;
-    }
-
-    public LocalDate getDataDeVencimento() {
-        return dataDeVencimento;
-    }
-
-    public void setDataDeVencimento(LocalDate dataDeVencimento) {
-        this.dataDeVencimento = dataDeVencimento;
-    }
-
-    public LocalDateTime getDataDePagamento() {
-        return dataDePagamento;
-    }
-
-    public void setDataDePagamento(LocalDateTime dataDePagamento) {
-        this.dataDePagamento = dataDePagamento;
-    }
 
 }
