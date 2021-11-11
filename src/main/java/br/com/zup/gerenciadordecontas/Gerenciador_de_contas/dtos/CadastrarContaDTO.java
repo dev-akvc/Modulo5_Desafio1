a7f09b19cfd6185ae849f7fa51b173b499dd150e
Anna Karolyne <dev.akvc@gmail.com>
2021-11-11 00:06:13 -0300

    Validar campos obrigatorios do cadastro

--- src/main/java/br/com/zup/gerenciadordecontas/Gerenciador_de_contas/dtos/CadastrarContaDTO.java
+++ src/main/java/br/com/zup/gerenciadordecontas/Gerenciador_de_contas/dtos/CadastrarContaDTO.java
@@ -5,14 +5,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 
 @NoArgsConstructor
 @Getter
 @Setter
 public class CadastrarContaDTO {
+    @NotBlank(message = "Nome da conta deve ser preenchido")
     private String nome;
+    @NotBlank(message = "Valor da conta deve ser preenchido")
     private double valor;
+    @NotBlank(message = "Tipo da conta deve ser preenchido")
     private TipoConta tipo;
     private LocalDate dataDeVencimento;
 
