cfa03716df24381c0efe7fb549ceea7f8cc5e0c0
Anna Karolyne <dev.akvc@gmail.com>
2021-11-11 10:41:04 -0300

    Adicionar validacao na data de vencimento

--- src/main/java/br/com/zup/gerenciadordecontas/Gerenciador_de_contas/dtos/CadastrarContaDTO.java
+++ src/main/java/br/com/zup/gerenciadordecontas/Gerenciador_de_contas/dtos/CadastrarContaDTO.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 
@@ -15,9 +16,11 @@ public class CadastrarContaDTO {
     @NotBlank(message = "Nome da conta deve ser preenchido")
     private String nome;
     @NotBlank(message = "Valor da conta deve ser preenchido")
+    @Min(value = 1, message = "Valor mínimo da conta é R$1,00")
     private double valor;
     @NotBlank(message = "Tipo da conta deve ser preenchido")
     private TipoConta tipo;
+    @NotBlank(message = "Data de vencimento da conta deve ser preenchida")
     private LocalDate dataDeVencimento;
 
 }
