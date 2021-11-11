a643ae06fc69392780432f1330d8875bef705f37
Anna Karolyne <dev.akvc@gmail.com>
2021-11-11 12:06:30 -0300

    Adicionar construtor para mensagem de erro de conta nao localizada

--- src/main/java/br/com/zup/gerenciadordecontas/Gerenciador_de_contas/exceptions/ContaNaoLocalizadaException.java
+++ src/main/java/br/com/zup/gerenciadordecontas/Gerenciador_de_contas/exceptions/ContaNaoLocalizadaException.java
@@ -1,4 +1,9 @@
 package br.com.zup.gerenciadordecontas.Gerenciador_de_contas.exceptions;
 
 public class ContaNaoLocalizadaException extends RuntimeException {
+
+    public ContaNaoLocalizadaException (String message) {
+        super(message);
+    }
+
 }
