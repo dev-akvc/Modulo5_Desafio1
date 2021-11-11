7a417d8cb796edc100ae6d9cb5f8b3940703cdba
Anna Karolyne <dev.akvc@gmail.com>
2021-11-11 10:38:28 -0300

    Criar classe para erro de validacao

--- src/main/java/br/com/zup/gerenciadordecontas/Gerenciador_de_contas/config/ErroDeValidacao.java
+++ src/main/java/br/com/zup/gerenciadordecontas/Gerenciador_de_contas/config/ErroDeValidacao.java
@@ -1,2 +1,13 @@
-package br.com.zup.gerenciadordecontas.Gerenciador_de_contas.config;public class ErroDeValidacao {
+package br.com.zup.gerenciadordecontas.Gerenciador_de_contas.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ErroDeValidacao {
+    private String campo;
+    private String mensagem;
 }
