63870c34896e1e073198586423ea54bf1103c319
Anna Karolyne <dev.akvc@gmail.com>
2021-11-11 09:59:51 -0300

    Criar classe para mensagens de erro

---
+++ src/main/java/br/com/zup/gerenciadordecontas/Gerenciador_de_contas/config/MensagemDeErro.java
@@ -0,0 +1,14 @@
+package br.com.zup.gerenciadordecontas.Gerenciador_de_contas.config;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class MensagemDeErro {
+    private String mensagemDeErro;
+
+}
